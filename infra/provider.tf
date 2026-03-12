5ce70deb0f55e065f3125e584ee90ef403a65c34
Warsame <warsameahmed95@gmail.com>
2026-03-12 17:21:51 +0000

    Added some basic infra files and folders

---
+++ infra/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.35.1"
+    }
+  }
+  backend "s3" {
+    bucket = "warsame-url-shortener"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+    
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
