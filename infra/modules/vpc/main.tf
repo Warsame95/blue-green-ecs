4e292ac31042ee38cd8e1a16d4d99551dd81d815
Warsame <warsameahmed95@gmail.com>
2026-03-13 11:30:28 +0000

    Added subnets and igw

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  
+
   tags = {
     Name = var.vpc_name
   }
@@ -9,3 +9,38 @@ resource "aws_vpc" "main" {
   }
   
 }
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+  tags_all = {
+    Name = "${var.vpc_name}-igw"
+  }
+    
+}
+
+resource "aws_subnet" "public" {
+    count = 2
+    vpc_id = aws_vpc.main.id
+    availability_zone = var.az[count.index]
+    cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index+1)
+
+    tags = {
+        Name = "${var.vpc_name}-public-${var.az[count.index]}"
+    }
+ }
+
+resource "aws_subnet" "private" {
+    count = 2
+    vpc_id = aws_vpc.main.id
+    availability_zone = var.az[count.index]
+    cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index+101)
+
+    tags = {
+        Name = "${var.vpc_name}-private-${var.az[count.index]}"
+    }
+
+ }
