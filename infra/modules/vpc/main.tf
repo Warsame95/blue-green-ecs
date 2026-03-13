e3751ec86947f94236e98007adfdccc73466246c
Warsame <warsameahmed95@gmail.com>
2026-03-13 11:32:18 +0000

    Created route tables and their associations

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -44,3 +44,39 @@ resource "aws_subnet" "private" {
     }
 
  }
+
+ resource "aws_route_table" "public-rtb" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+
+    tags = {
+        Name = "${var.vpc_name}-public-rtb"
+    }
+}
+
+resource "aws_route_table" "private-rtb" {
+    vpc_id = aws_vpc.main.id
+
+
+    tags = {
+            Name = "${var.vpc_name}-private-rtb"
+        }
+}
+
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public-rtb.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = 2
+  
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private-rtb.id
+}
