050c1a17a8a36f144d7ad2ebe9fd39d78d00bd7c
Warsame <warsameahmed95@gmail.com>
2026-03-16 14:23:50 +0000

    Added an A record resource

--- infra/modules/dns/main.tf
+++ infra/modules/dns/main.tf
@@ -2,3 +2,16 @@ resource "aws_route53_zone" "primary" {
   name = var.domain
 }
 
+resource "aws_route53_record" "this" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = var.domain
+  type    = "A"
+
+  alias {
+    name                   = var.dns_name
+    zone_id                = var.alb_zone_id
+    evaluate_target_health = true
+  }
+
+  
+}
