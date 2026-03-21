bf34e01af5889bd112009fb8626cead373857122
Warsame <warsameahmed95@gmail.com>
2026-03-21 10:02:13 +0000

    created task role with its permissions

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -18,3 +18,44 @@ resource "aws_iam_role_policy_attachment" "execution_policy" {
   role = aws_iam_role.execution_role.name
   policy_arn = var.execution_policy_arn
 }
+
+resource "aws_iam_role" "task_role" {
+    name = var.task_role_name
+
+    assume_role_policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = { Service = "ecs-tasks.amazonaws.com" }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+}
+
+# This data source simply creates the set of permissions for dynamodb
+data "aws_iam_policy_document" "ecs_task_app" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:Query",
+      "dynamodb:Scan"
+    ]
+    resources = [
+      var.dynamodb_table_arn
+    ]
+  }
+}
+
+# attaching the task role to the dynamodb policy
+resource "aws_iam_role_policy" "dynamodb_task" {
+  name = "dynamodb-task"
+  role = aws_iam_role.task_role.arn
+  policy = data.aws_iam_policy_document.ecs_task_app.json
+}
--- infra/modules/iam/variables.tf
+++ infra/modules/iam/variables.tf
@@ -2,6 +2,14 @@ variable "execution_role_name" {
   type = string
 }
 
+variable "task_role_name" {
+  type = string
+}
+
 variable "execution_policy_arn" {
+  type = string
+}
+
+variable "dynamodb_table_arn" {
   type = string
 }
