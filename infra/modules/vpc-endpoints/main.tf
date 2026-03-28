91c9f9455f7e24ad511fd0981c24f402f67107da
Warsame <warsameahmed95@gmail.com>
2026-03-28 14:29:39 +0000

    referenced var.region into the service name property values

--- infra/modules/vpc-endpoints/main.tf
+++ infra/modules/vpc-endpoints/main.tf
@@ -1,13 +1,13 @@
 resource "aws_vpc_endpoint" "ecr-api" {
   vpc_id       = var.vpc_id
-  service_name = "com.amazonaws.eu-west-2.ecr.api"
+  service_name = "com.amazonaws.${var.region}.api"
   vpc_endpoint_type = "Interface"
   subnet_ids = var.private_subnet_ids
 }
 
 resource "aws_vpc_endpoint" "ecr-dkr" {
   vpc_id       = var.vpc_id
-  service_name = "com.amazonaws.eu-west-2.ecr.dkr"
+  service_name = "com.amazonaws.${var.region}.ecr.dkr"
   vpc_endpoint_type = "Interface"
   subnet_ids = var.private_subnet_ids
 }
@@ -15,13 +15,13 @@ resource "aws_vpc_endpoint" "ecr-dkr" {
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = var.vpc_id
   vpc_endpoint_type = "Gateway"
-  service_name = "com.amazonaws.eu-west-2.s3"
+  service_name = "com.amazonaws.${var.region}.s3"
   route_table_ids = [var.private_rtb_id]
 }
 
 resource "aws_vpc_endpoint" "dynamodb" {
   vpc_id       = var.vpc_id
   vpc_endpoint_type = "Gateway"
-  service_name = "com.amazonaws.eu-west-2.dynamodb"
+  service_name = "com.amazonaws.${var.region}.dynamodb"
   route_table_ids = [var.private_rtb_id]
 }
