75f25d7f3ce5e1957a21fe2184d4291940896678
Warsame <warsameahmed95@gmail.com>
2026-03-28 18:03:05 +0000

    Added cloudwatch logs endpoint

--- infra/modules/vpc-endpoints/main.tf
+++ infra/modules/vpc-endpoints/main.tf
@@ -25,3 +25,10 @@ resource "aws_vpc_endpoint" "dynamodb" {
   service_name = "com.amazonaws.${var.region}.dynamodb"
   route_table_ids = [var.private_rtb_id]
 }
+
+resource "aws_vpc_endpoint" "cloudwatch" {
+  vpc_id = var.vpc_id
+  vpc_endpoint_type = "Interface"
+  service_name = "com.amazonaws.${var.region}.logs"
+  subnet_ids = var.private_subnet_ids
+}
