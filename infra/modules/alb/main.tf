a86404c4ab5ed92629c74bc302e61c8e8aeaaafb
Warsame <warsameahmed95@gmail.com>
2026-04-06 10:57:48 +0100

    Added both target groups to the listener

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -17,7 +17,16 @@ resource "aws_lb_listener" "https" {
 
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
+    forward {
+      target_group {
+        arn    = aws_lb_target_group.blue.arn
+        weight = 1
+      }
+      target_group {
+        arn    = aws_lb_target_group.green.arn
+        weight = 0
+      }
+    }
   }
 }
 
