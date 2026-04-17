20855ce0d5049aa13e5a60b4a539dc978e2def3d
Warsame <warsameahmed95@gmail.com>
2026-04-17 15:25:52 +0100

    Add a http listener to redirect traffic back to https

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -25,6 +25,22 @@ resource "aws_lb_listener" "https" {
   }
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_lb_listener_rule" "production_listener_rule" {
   listener_arn = aws_lb_listener.https.arn
   priority = 100
